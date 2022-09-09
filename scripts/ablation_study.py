965c06f14a5a48779adbb53925b59fc1bba6322c
Arnav Thareja <athareja@cs.washington.edu>
2022-09-09 00:27:40 -0700

    Update ablation script

--- scripts/ablation_study.py
+++ scripts/ablation_study.py
@@ -24,7 +24,7 @@ def get_m_std(x):
 output = []
 labels = config["sys_name"]
 colors = ["red", "purple", "blue", "green"]
-bar_width = 0.15
+block_tolerance = 0.15
 
 sys_type = []
 name_dict = config["sys_name"]
@@ -33,18 +33,16 @@ for name in name_dict:
 
 for name in sys_type:
     sys = np.load(base_dir + "/" + name + "/output_raw.npy", allow_pickle=True)
-    success = sys[0,:].mean()
-    collision = sys[1,:].mean()
     success_filter = (sys[0, :] > 0) & (sys[1, :] == 0)
     cte_m, cte_std = get_m_std(sys[2,success_filter])
     block_e_m, block_e_std = get_m_std(sys[3,success_filter])
     time_m, time_std = get_m_std(sys[4,success_filter])
     min_dist_m, min_dist_std = get_m_std(sys[5,success_filter & (sys[5, :] > 0)])
-    block_fail = len(np.where(sys[3,:] > 0.15)[0]) / len(sys[3, :])
+    block_fail = len(np.where(sys[3,:] > block_tolerance)[0]) / len(sys[3, :])
     makespan_m, makespan_std = get_m_std(sys[4,success_filter])
+    success_rate = (sys[3, success_filter] < block_tolerance).sum() / len(sys[0, :])
     print("sys name: ", name)
-    print("success: ", max(success - collision - block_fail,0), "valid for clcbs+pp")  # use this for CLCBS+PP
-    print("success: ", max(success - block_fail,0), "valid for every other system with collision avoidance")  # use this for everything else.
+    print("success: ", success_rate)
     print("cte m/std: ", cte_m, cte_std)
     print("block error m/std: ", block_e_m, block_e_std)
     print("min dist m/std: ", min_dist_m, min_dist_std)
